a5c643c6cd42cea77c3658c55abaf82c61c61a86
Maria Averina <amaria.webdev@gmail.com>
2021-08-08 19:25:47 +0300

    исправить ошибки linter

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,94 +1,94 @@
 const Card = require('../models/card');
 
 module.exports.getCards = (req, res) => {
-    Card.find({})
-        .then((card) => {
-            if (!card) {
-                res.status(404).send({ message: 'Нет пользователя с таким id' });
-                return;
-            }
-            res.send(card);
-        })
-        .catch((err) => {
-            if (err.message === 'CastError') {
-                res.status(400).send({ message: 'Переданы некорректные данные' });
-            }
-            res.status(500).send({ message: 'Запрашиваемый ресурс не найден' });
-        });
+  Card.find({})
+    .then((card) => {
+      if (!card) {
+        res.status(404).send({ message: 'Нет пользователя с таким id' });
+        return;
+      }
+      res.send(card);
+    })
+    .catch((err) => {
+      if (err.message === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      }
+      res.status(500).send({ message: 'Запрашиваемый ресурс не найден' });
+    });
 };
 
 module.exports.createCard = (req, res) => {
-    const { name, link } = req.body;
-    Card.create({ name, link, owner: req.user._id })
-        .then((card) => {
-            res.status(200).send(card);
-        })
-        .catch((err) => {
-            if (err.message === 'CastError') {
-                res.status(400).send({ message: 'Переданы некорректные данные' });
-            }
-            res.status(500).send({ message: 'Запрашиваемый ресурс не найден' });
-        });
+  const { name, link } = req.body;
+  Card.create({ name, link, owner: req.user._id })
+    .then((card) => {
+      res.status(200).send(card);
+    })
+    .catch((err) => {
+      if (err.message === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      }
+      res.status(500).send({ message: 'Запрашиваемый ресурс не найден' });
+    });
 };
 
 module.exports.deleteCard = (req, res) => {
-    Card.findByIdAndRemove(req.params._id)
-        .then((card) => {
-            if (!card) {
-                res.status(404).send({ message: 'Нет пользователя с таким id' });
-                return;
-            }
-            res.send(card);
-        })
-        .catch((err) => {
-            if (err.name === 'CastError') {
-                res.status(400).send({ message: 'Переданы некорректные данные' });
-            }
-            res.status(500).send({ message: 'Запрашиваемый ресурс не найден' });
-        });
+  Card.findByIdAndRemove(req.params._id)
+    .then((card) => {
+      if (!card) {
+        res.status(404).send({ message: 'Нет пользователя с таким id' });
+        return;
+      }
+      res.send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      }
+      res.status(500).send({ message: 'Запрашиваемый ресурс не найден' });
+    });
 };
 
 module.exports.likeCard = (req, res) => {
-    Card.findByIdAndUpdate(
-            req.params._id, {
-                $addToSet: { likes: req.params._id },
-            }, { new: true },
-        )
-        .then((card) => {
-            if (!card) {
-                res.status(404).send({ message: 'Нет пользователя с таким id' });
-                return;
-            }
-            res.send(card);
-        })
-        .catch((err) => {
-            if (err.message === 'CastError') {
-                res.status(400).send({ message: 'Переданы некорректные данные' });
-            }
-            res.status(500).send({ message: 'Запрашиваемый ресурс не найден' });
-        });
+  Card.findByIdAndUpdate(
+    req.params._id, {
+      $addToSet: { likes: req.params._id },
+    }, { new: true },
+  )
+    .then((card) => {
+      if (!card) {
+        res.status(404).send({ message: 'Нет пользователя с таким id' });
+        return;
+      }
+      res.send(card);
+    })
+    .catch((err) => {
+      if (err.message === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      }
+      res.status(500).send({ message: 'Запрашиваемый ресурс не найден' });
+    });
 };
 
 module.exports.dislikeCard = (req, res) => {
-    Card.findByIdAndUpdate(
-            req.params._id, {
-                $pull: { likes: req.params._id },
-            }, { new: true },
-        )
-        .then((card) => {
-            if (!card) {
-                res.status(404).send({ message: 'Нет пользователя с таким id' });
-                return;
-            }
-            res.send(card);
-        })
-        .catch((err) => {
-            if (err.name === 'CastError') {
-                res.status(400).send({ message: 'Переданы некорректные данные' });
-            }
-            if (err.name === 'NotFound') {
-                res.status(404).send({ message: 'Карточки с таким id не найдены' });
-            }
-            res.status(500).send({ message: 'Запрашиваемый ресурс не найден' });
-        });
+  Card.findByIdAndUpdate(
+    req.params._id, {
+      $pull: { likes: req.params._id },
+    }, { new: true },
+  )
+    .then((card) => {
+      if (!card) {
+        res.status(404).send({ message: 'Нет пользователя с таким id' });
+        return;
+      }
+      res.send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      }
+      if (err.name === 'NotFound') {
+        res.status(404).send({ message: 'Карточки с таким id не найдены' });
+      }
+      res.status(500).send({ message: 'Запрашиваемый ресурс не найден' });
+    });
 };
